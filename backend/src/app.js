5a1552bce9ef20f726a04b297c2c3807dfedb195
HBDMS-cloud <kadiritairu503@gmail.com>
2026-07-15 20:08:04 +0100

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,14 @@
+const http = require("http");
 
+const app = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  });
+
+  res.end(JSON.stringify({
+    status: "success",
+    message: "Welcome to HYPA Brotherhood Digital Management System (HBDMS) API"
+  }));
+});
+
+module.exports = app;
