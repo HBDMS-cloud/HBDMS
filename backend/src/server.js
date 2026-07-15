2b5ab2c62a128e6a786da5b2522dfe2251be5e38
HBDMS-cloud <kadiritairu503@gmail.com>
2026-07-15 19:56:52 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,12 @@
-place file 
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Welcome to HBDMS Backend!");
+});
+
+server.listen(PORT, () => {
+  console.log(`HBDMS Backend running on port ${PORT}`);
+});place file 
