1a74bc25861a2338e6a6b1b673e1ed0534dc24d3
HBDMS-cloud <kadiritairu503@gmail.com>
2026-07-18 15:24:44 +0100

    Update database.js

--- backend/src/src/config/database.js
+++ backend/src/src/config/database.js
@@ -1,1 +1,11 @@
+const { Pool } = require("pg");
 
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "your_password",
+  database: "hbdms",
+});
+
+module.exports = pool;
