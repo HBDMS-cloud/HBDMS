20720bc5f9aa684da4a9d4fe8fbb8ef37c62ca97
HBDMS-cloud <kadiritairu503@gmail.com>
2026-07-19 14:31:13 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
-const app = require("./app");
+const app = require("../app");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`HBDMS Backend running on port ${PORT}`);
+  console.log(`HBDMS Server running on port ${PORT}`);
 });
