56edf940fb901e846603602d017384412e7568d0
HBDMS-cloud <kadiritairu503@gmail.com>
2026-07-19 14:27:53 +0100

    Set up Express server with member routes

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-file folder 
+const express = require("express");
+
+const memberRoutes = require("./src/src/routes/memberRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/members", memberRoutes);
+
+app.get("/", (req, res) => {
+  res.send("HYPA Brotherhood Digital Management System API");
+});
+
+module.exports = app;
